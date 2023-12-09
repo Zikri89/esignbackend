b2709dc6ec46bf9d0d56c2eecb0f4b22c0e2783e
zikri89 <asalngoding89@gmail.com>
2023-12-09 23:08:00 +0700

    form

--- api/models/FormDataPasien.js
+++ api/models/FormDataPasien.js
@@ -10,5 +10,22 @@ module.exports = {
       columnType: 'json',
       required: true,
     },
+    isDeleted: {
+      type: 'boolean',
+      columnName: 'isDeleted',
+      defaultsTo: false,
+    },
+    deletedBy: {
+      type: 'string',
+      columnName: 'deletedBy',
+    },
+    deletedAt: {
+      type: 'ref',
+      columnName: 'deletedAt',
+    },
+    status: {
+      type: 'string',
+      columnName: 'status',
+    },
   }
 };
